3c35453a308390803c580c967c8b9337fb64fbec
kaustavray21 <kaustav.raysinha@gmail.com>
2025-09-20 17:59:42 +0530

    made changes in the UI

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class LoginHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    login_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} logged in at {self.login_timestamp}'
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
+from .models import LoginHistory
 
 def home(request):
     return render(request, 'home.html')
@@ -27,6 +28,7 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
+                LoginHistory.objects.create(user=user)
                 return redirect('dashboard')
     else:
         form = AuthenticationForm()
@@ -38,4 +40,10 @@ def logout_view(request):
 
 @login_required
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    login_history = LoginHistory.objects.filter(user=request.user).order_by('-login_timestamp')
+    total_logins = login_history.count()
+    context = {
+        'login_history': login_history,
+        'total_logins': total_logins
+    }
+    return render(request, 'dashboard.html', context)
